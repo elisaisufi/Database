119874de5a366b2c257ed1049be8a04d269ae452
elisaisufi <elisa.isufi.2024@gmail.com>
2026-04-22 16:45:21 +0200

    db_DistinctQuery.sql

--- db_DistinctQuery.sql
+++ db_DistinctQuery.sql
@@ -1,5 +1,3 @@
-USE internship;
-
 -- Part 3 Distinct Queries
 
 -- Unique programming languages
@@ -11,13 +9,13 @@ SELECT DISTINCT points
 FROM student;
 
 -- Retreive 3 students from 'Intro to Artificial Intelligence', where you order from youngest to oldest
-SELECT student_id, name
+SELECT student_id, student_name
 FROM student
 ORDER BY birth_date asc
 LIMIT 3;
 
 -- Retreive 4 students that have more than 80 points
-SELECT student_id, name 
+SELECT student_id, student_name 
 FROM student
 WHERE points > 80
 LIMIT 4;
@@ -28,7 +26,7 @@ FROM course
 LIMIT 3 OFFSET 3;
 
 -- Skip the first 4 students and show the next 3, ordering by points
-SELECT name, points
+SELECT student_name, points
 FROM student
 ORDER BY points DESC
 LIMIT 3 OFFSET 3;
